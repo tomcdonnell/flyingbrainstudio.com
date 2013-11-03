fa1a6b9dc3f9deb1e4362e2640801f8c13c13bea
Tom McDonnell <tomcdonnell@gmail.com>
2013-11-04 02:13:54 +1100

    Added link to facebook page.

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ UtilsHtml::echoHtmlScriptAndLinkTagsForJsAndCssFiles
   <div id='main-content-div'>
    <h1><img src='images/flying_brain_studio_banner.png' alt='Flying Brain Studio'/></h1>
    <p>Flying Brain Studio is a recording studio in Tecoma, Melbourne, Australia.</p>
+   <p>For more information contact <a href='http://facebook.com/luketylim'>Luke Tylim</a>.</p>
    <p>This site is under construction.</p>
   </div>
  </body>
