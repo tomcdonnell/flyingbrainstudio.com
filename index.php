33c3ca3b4a4a1d65c449d85bb30dc2beee99066f
Tom McDonnell <tomcdonnell@gmail.com>
2013-11-04 02:07:12 +1100

    Added background image.

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
+<?php
+require_once 'lib/tom/php/utils/UtilsHtml.php';
+?>
 <!DOCTYPE html>
 <html>
- <head><title>Flying Brain Studio</title></head>
+ <head>
+  <title>Flying Brain Studio</title>
+<?php
+UtilsHtml::echoHtmlScriptAndLinkTagsForJsAndCssFiles
+(
+   array('css/style.css'),
+   array()
+);
+?>
+ </head>
  <body>
-  <h1>Flying Brain Studio</h1>
-  <p>Site under construction.</p>
+  <div id='main-content-div'>
+   <h1><img src='images/flying_brain_studio_banner.png' alt='Flying Brain Studio'/></h1>
+   <p>Flying Brain Studio is a recording studio in Tecoma, Melbourne, Australia.</p>
+   <p>This site is under construction.</p>
+  </div>
  </body>
 </html>
